dec89344ebd463fc4fa15d4330dd38d2c0e27ca0
chrisraro <rarochristian029@gmail.com>
2025-10-30 23:52:46 +0800

    privacy and terms page fix build error

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
-import { LpNavbar1 } from "@/components/pro-blocks/landing-page/navbars/navbar-1";
+import { LpNavbar1 } from "@/components/pro-blocks/landing-page/lp-navbars/lp-navbar-1";
 import { Footer1 } from "@/components/pro-blocks/landing-page/footers/footer-1";
 import { Logo } from "@/components/pro-blocks/logo";
 
